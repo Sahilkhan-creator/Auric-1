1bfffd89381da4ef4f4ab7eb3d09c133199c9d71
Sahilkhan-creator <mineblade3535@gmail.com>
2025-02-12 10:20:17 +0530

    Update and rename Zcvbb to script.js

---
+++ script.js
@@ -0,0 +1,7 @@
+function showLoginForm() {
+    document.getElementById("loginForm").style.display = "block";
+}
+function login() {
+    alert("Login Successful!");
+    window.location.href = "homepage.html";
+}
